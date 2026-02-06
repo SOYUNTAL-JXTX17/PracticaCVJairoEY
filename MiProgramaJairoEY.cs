b917a4fb822599ada137a6062113b9d03228f382
Jairo Encabo Yllesca <a25jairoencyll@iesluiscarrillodesotomayor.org>
2026-02-06 08:50:23 +0100

    Creación del Fichero MiProgramaJairoEY.cs
    Fichero que contendrá la estructura del programa

---
+++ MiProgramaJairoEY.cs
@@ -0,0 +1,10 @@
+namespace PracticaCVJairoEY
+{
+    internal class MiProgramaJairoEY
+    {
+      static void Main(string[] args)
+      {
+        Console.Writeline("Hola mundo");
+      }
+    }
+}
